58cd795794ddecf706842ad79b185e2ba9bb4d22
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-01-23 16:46:47 +0530

    Added Create City api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const cityRoutes= require('./city-routes')
+const airplaneRoutes = require("./airplane-routes");
 const { InfoController } = require('../../controllers');
 const router = express.Router();
 
-const airplaneRoutes = require('./airplane-routes');
 router.use('/airplanes', airplaneRoutes);
 
 router.get('/info', InfoController.info);
+
+router.use ('/cities', cityRoutes)
 
 module.exports = router;
