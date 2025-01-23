e29d694ca080ac1526b0898428ecbdd13d333000
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-01-23 13:40:39 +0530

    Added get airplane by id api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -1,4 +1,6 @@
+const { StatusCodes } = require("http-status-codes");
 const { Logger } = require("../config");
+const AppError = require("../utils/errors/app-error");
 
 class CrudRepository {
   constructor(model) {
@@ -30,6 +32,12 @@ class CrudRepository {
   async get(data) {
     try {
       const response = await this.model.findByPk(data);
+      if (!response) {
+        throw new AppError(
+          "Not able to find the resource",
+          StatusCodes.NOT_FOUND
+        );
+      }
       return response;
     } catch (error) {
       Logger.error("Something went wrong in Crud Repo: get");
@@ -51,7 +59,7 @@ class CrudRepository {
         where: {
           id: id,
         },
-      }); 
+      });
       return response;
     } catch (error) {
       Logger.error("Something went wrong in Crud Repo: update");
