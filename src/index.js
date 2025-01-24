29f68018d94be95d018dcc0fd7f270599417e23f
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-01-24 19:33:52 +0530

    Added flights model with foreign key constraint defined during model definition

--- src/index.js
+++ src/index.js
@@ -11,17 +11,4 @@ app.use(express.urlencoded({extended:true}));
 app.use('/api', apiRoutes);
 app.listen(ServerConfig.PORT,async  () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-  const { City,Airport } = require("./models");
-//   const bengaluru = await City.findByPk(1, { include: { model: Airport } });
-//   const airport = await Airport.create({
-//     name: "Kempegowda Airport",
-//     code: "BLR",
-//     cityId: 1,
-//   });
-//   const dbpairport = await bengaluru.createAirport({name: 'Huballi Airport', code: 'HBL'});
-    await City.destroy({
-      where: {
-        id: 1,
-      },
-    });
 });
